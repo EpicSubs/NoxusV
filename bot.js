ee31251f88766c94f5d4d4942f94e375584fb374
Glitch (hello-express) <none>
2020-10-06 09:53:47 +0000

    💆🛳 Checkpoint ./events/message.js:22377029/4442 ./events/ready.js:22377029/3158 ./ayarlar.json:22377029/152 ./README.md:22377029/1193 ./package.json:22377029/11689 ./bot.js:22377029/25724 ./util/Util.js:22377029/10736 ./util/EventLoader.js:22377029/571 ./json.sqlite:22377029/2

---
+++ bot.js
@@ -0,0 +1,305 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + "Noxus a abone ol");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  //dokunma al
+  http.get(`https://sugared-abalone-timimus.glitch.me/`);
+}, 280000);
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const ayarlar = require("./ayarlar.json");
+const chalk = require("chalk");
+const Canvas = require("canvas"),
+  Image = Canvas.Image,
+  Font = Canvas.Font,
+  path = require("path");
+const snekfetch = require("snekfetch");
+const fs = require("fs");
+const DBL = require("dblapi.js");
+const YouTube = require("simple-youtube-api");
+const queue = new Map();
+const ytdl = require("ytdl-core");
+const generator = require("generate-password");
+const math = require("math-expression-evaluator");
+const db = require("wio.db");
+const moment = require("moment");
+const ms = require("parse-ms");
+const GIFEncoder = require("gifencoder");
+require("moment-duration-format");
+require("./util/eventLoader")(client);
+
+var prefix = ayarlar.prefix;
+
+const log = message => {
+  console.log(`[${moment().format("YYYY-MM-DD HH:mm:ss")}] ${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.elevation = message => {
+  if (!message.guild) {
+    return;
+  }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
+};
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+// client.on('debug', e => {
+//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+// });
+
+client.on("warn", e => {
+  console.log(chalk.bgYellow(e.replace(regToken, "that was redacted")));
+});
+
+client.on("error", e => {
+  console.log(chalk.bgRed(e.replace(regToken, "that was redacted")));
+});
+
+////////KOMUTLAR BURDAN SONRA
+
+client.login(ayarlar.token);
+//-------------—------------—---—————-------KOMUTLAR-----------------------------------------//
+client.on("ready", () => {
+  client.channels.get("759456807365247056").join();
+});
+client.on("message", msg => {
+  var dm = client.channels.get("759764386927345714");
+  if (msg.channel.type === "dm") {
+    if (msg.author.id === client.user.id) return;
+    const botdm = new Discord.RichEmbed()
+      .setTitle(`${client.user.username} Gelen Dm`)
+      .setTimestamp()
+      .setColor("BLUE")
+      .setThumbnail(`${msg.author.avatarURL}`)
+      .addField("Mesaj Atan", msg.author.tag)
+      .addField("Mesaj Atanın İDsi", msg.author.id)
+      .addField("Gönderilen Mesaj", msg.content);
+
+    dm.send(botdm);
+  }
+  if (msg.channel.bot) return;
+});
+//glitch.com/edit/#!/join/6db097ce-066e-4ffa-a
+client.on("message", message => {
+  if (message.content.toLowerCase() === "<@590847090205392896>") {
+    message.delete();
+    message.channel
+      .send(message.author + ",  Yapımcımı Etiketleme")
+      .then(message => message.delete(5000));
+  }
+});
+client.on("message", async (msg, member, guild) => {
+  let i = await db.fetch(`saas_${msg.guild.id}`);
+  if (i === "açık") {
+    if (msg.content.toLowerCase() === "sa") {
+      msg.reply("Aleyküm Selam, Hoşgeldin ");
+    }
+  }
+});
+
+client.on("message", async (msg, member, guild) => {
+  let i = await db.fetch(`saas_${msg.guild.id}`);
+  if (i === "açık") {
+    if (msg.content.toLowerCase() === "hi") {
+      msg.reply("Hi welcome ");
+    }
+  }
+});
+client.on("message", async msg => {
+  if (msg.author.bot) return;
+  if (msg.channel.type === "dm") return;
+
+  let i = await db.fetch(`reklamFiltre_${msg.guild.id}`);
+  if (i == "acik") {
+    const reklam = [
+      "discord.app",
+      "discord.gg",
+      "invite",
+      "discordapp",
+      "discordgg",
+      ".com",
+      ".net",
+      ".xyz",
+      ".tk",
+      ".pw",
+      ".io",
+      ".me",
+      ".gg",
+      "www.",
+      "https",
+      "http",
+      ".gl",
+      ".org",
+      ".com.tr",
+      ".biz",
+      ".party",
+      ".rf.gd",
+      ".az"
+    ];
+    if (reklam.some(word => msg.content.toLowerCase().includes(word))) {
+      try {
+        if (!msg.member.hasPermission("ADMINISTRATOR")) {
+          msg.delete();
+          let embed = new Discord.RichEmbed()
+            .setColor(0xffa300)
+            .setFooter("Reklam engellendi.", client.user.avatarURL)
+            .setAuthor(
+              msg.guild.owner.user.username,
+              msg.guild.owner.user.avatarURL
+            )
+            .setDescription(
+              "Reklam sistemi, " +
+                `***${msg.guild.name}***` +
+                " adlı sunucunuzda reklam yakaladım."
+            )
+            .addField(
+              "Reklamı yapan kişi",
+              "Kullanıcı: " + msg.author.tag + "\nID: " + msg.author.id,
+              true
+            )
+            .addField("Engellenen mesaj", msg.content, true)
+            .setTimestamp();
+          msg.guild.owner.user.send(embed);
+          return msg.channel
+            .send(`${msg.author.tag}, Reklam Yapmak Yasak Bunu Biliyorsun.`)
+            .then(msg => msg.delete(25000));
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+  if (!i) return;
+});
+
+client.on("message", async msg => {
+  if (msg.author.bot) return;
+  if (msg.channel.type === "dm") return;
+
+  let i = await db.fetch(`küfürFiltre_${msg.guild.id}`);
+  if (i == "acik") {
+    const küfür = [
+      "amk",
+      "amq",
+      "orospu",
+      "piç",
+      "pıç",
+      "göt",
+      "got",
+      "sg",
+      "aq",
+      "AMK",
+      "AMQ",
+      "OROSPU",
+      "PİÇ"
+    ];
+    if (küfür.some(word => msg.content.toLowerCase().includes(word))) {
+      try {
+        if (!msg.member.hasPermission("ADMINISTRATOR")) {
+          msg.delete();
+          let embed = new Discord.RichEmbed()
+            .setColor(0xffa300)
+            .setFooter("  Küfür Engel.", client.user.avatarURL)
+            .setAuthor(
+              msg.guild.owner.user.username,
+              msg.guild.owner.user.avatarURL
+            )
+            .setDescription(
+              "Küfür sistemi " +
+                `***${msg.guild.name}***` +
+                " adlı sunucunuzda küfür yakaladım."
+            )
+            .addField(
+              "Küfür eden kişi",
+              "Kullanıcı: " + msg.author.tag + "\nID: " + msg.author.id,
+              true
+            )
+            .addField("Engellenen mesaj", msg.content, true)
+            .setTimestamp();
+          msg.guild.owner.user.send(embed);
+          return msg.channel
+            .send(`${msg.author.tag}, Küfür Etmek Yasak Bunu Biliyorsun.`)
+            .then(msg => msg.delete(25000));
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+  if (!i) return;
+});
