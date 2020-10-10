7178e6186c4c9c9a0710abb2fb514957ba737cca
Glitch (hello-express) <none>
2020-10-10 11:11:31 +0000

    🛢🖇 Checkpoint ./ayarlar.json:22377029/118 ./bot.js:22377029/51

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const http = require("http");
 const express = require("express");
 const app = express();
 app.get("/", (request, response) => {
-  console.log(Date.now() + "Noxus a abone ol");
+  console.log(Date.now() + "Bot Başarı İle Aktif Edildi");
   response.sendStatus(200);
 });
 app.listen(process.env.PORT);
