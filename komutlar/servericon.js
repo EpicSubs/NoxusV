d4edafc8910e1a999718685ded202a68ca4c6025
Glitch (hello-express) <none>
2020-10-10 07:42:26 +0000

    🕋👐 Checkpoint ./komutlar/servericon.js:22377029/3275 ./komutlar/roleinfo.js:22377029/3668 ./komutlar/yardım.js:22377029/56 ./komutlar/restgeleuye.js:22377029/10692 ./komutlar/duyuru.js:22377029/1873

--- komutlar/servericon.js
+++ komutlar/servericon.js
@@ -1,23 +1,37 @@
 const Discord = require("discord.js");
 
-exports.run = function(client, message) {
-  const embed = new Discord.RichEmbed()
-    .setDescription("**SUNUCU ICONU**")
-    .setImage(message.guild.iconURL);
-
-  message.channel.send(embed);
+exports.run = (client, message, params) => {
+  if (!message.guild) {
+    const ozelmesajuyari = new Discord.RichEmbed()
+      .setColor(0xff0000)
+      .setTimestamp()
+      .setAuthor(message.author.username, message.author.avatarURL)
+      .addField(
+        ":warning: Uyarı :warning:",
+        "`sunucuresmi` adlı komutu özel mesajlarda kullanamazsın."
+      );
+    return message.author.sendEmbed(ozelmesajuyari);
+  }
+  if (message.channel.type !== "dm") {
+    const sunucubilgi = new Discord.RichEmbed()
+      .setAuthor(message.guild.name)
+      .setColor(3447003)
+      .setTimestamp()
+      .setDescription("")
+      .setImage(`${message.guild.iconURL} `);
+    return message.channel.sendEmbed(sunucubilgi);
+  }
 };
 
 exports.conf = {
   enabled: true,
-  guildOnly: true,
-  aliases: [],
+  guildOnly: false,
+  aliases: ["sunucu-pp"],
   permLevel: 0
 };
 
 exports.help = {
-  name: "servericon",
-  description: "Serverin iconunu gösterir",
-  usage: "servericon"
+  name: "sunucuresmi",
+  description: "Sunucu Resminin Linkini Atar.",
+  usage: "sunucuresmi"
 };
-  
