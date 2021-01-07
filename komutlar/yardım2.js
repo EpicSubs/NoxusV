677418c44338ee76664e94e4786f96a965052b52
Glitch (hello-express) <none>
2021-01-07 15:36:23 +0000

    👵🌸 Checkpoint ./komutlar/yardım2.js:21810194/10241

--- komutlar/yardım2.js
+++ komutlar/yardım2.js
@@ -1,50 +1,23 @@
 const Discord = require("discord.js");
 
 exports.run = (client, message, args) => {
-  message.author.avatarURL())
-  .setDescription("")
-      //Filter
-      const backwardsFilter = (reaction, user) =>
-        reaction.emoji.name === "⬅" && user.id === message.author.id;
-      const forwardsFilter = (reaction, user) =>
-        reaction.emoji.name === "➡" && user.id === message.author.id;
+  if (!message.guild) {
+    const ozelmesajuyari = new Discord.MessageEmbed()
+      .setColor(0x2488e7)
+      .setTimestamp()
+      .setAuthor(message.author.username, message.author.avatarURL())
+      .addField("Selamun Aleyküm");
+  }
+  exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ["help", "y", "h"],
+    permLevel: 0
+  };
 
-      const backwards = msg.createReactionCollector(backwardsFilter, {
-        time: 100000
-      });
-      const forwards = msg.createReactionCollector(forwardsFilter, {
-        time: 100000
-      });
-
-      forwards.on("collect", r => {
-        if (page === pages.length) return;
-        page++;
-        embed.setDescription(pages[page - 1]);
-        embed.setColor("RANDOM");
-        embed.setFooter(`Sayfa ${page} / ${pages.length}`);
-        msg.edit(embed);
-      });
-      backwards.on("collect", r => {
-        if (page === 1) return;
-        page--;
-        embed.setColor("RANDOM");
-        embed.setDescription(pages[page - 1]);
-        embed.setFooter(`Sayfa ${page} / ${pages.length}`);
-        msg.edit(embed);
-      });
-    });
-  });
-};
-
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: ["help", "y", "h"],
-  permLevel: 0
-};
-
-exports.help = {
-  name: "yardım",
-  description: "Yardım Listesini Gösterir",
-  usage: "yardım"
+  exports.help = {
+    name: "yardımü",
+    description: "Yardım Listesini Gösterir",
+    usage: "yardımü"
+  };
 };
