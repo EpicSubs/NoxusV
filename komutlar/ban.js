061fdac76f9810d47c26612f3c6117ec1bf9d313
Glitch (hello-express) <none>
2021-01-07 15:00:35 +0000

    🗼🐊 Checkpoint ./komutlar/ban.js:21810194/3096

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -1,32 +1,36 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
 exports.run = (client, message, args) => {
   if (!message.guild) {
-  const ozelmesajuyari = new Discord.MessageEmbed()
-  .setColor(0xFF0000)
-  .setTimestamp()
-  .setAuthor(message.author.username, message.author.avatarURL())
-  .addField(':warning: Uyarı :warning:', 'ban adlı komutu özel mesajlarda kullanamazsın.')
-  return message.author.send(ozelmesajuyari); }
-  let guild = message.guild
-  let reason = args.slice(1).join(' ');
+    const ozelmesajuyari = new Discord.MessageEmbed()
+      .setColor(0xff0000)
+      .setTimestamp()
+      .setAuthor(message.author.username, message.author.avatarURL())
+      .addField(
+        ":warning: Uyarı :warning:",
+        "ban adlı komutu özel mesajlarda kullanamazsın."
+      );
+    return message.author.send(ozelmesajuyari);
+  }
+  let guild = message.guild;
+  let reason = args.slice(1).join(" ");
   let user = message.mentions.users.first();
-  let modlog = guild.channels.cache.find('name', 'mod-log');
-  if (!modlog) return message.reply('mod-log kanalını bulamıyorum.');
-  if (reason.length < 1) return message.reply('Ban sebebini yazmalısın.');
-  if (message.mentions.users.cache.size < 1) return message.reply('Kimi banlayacağını yazmalısın.').catch(console.error);
+  let modlog = guild.channels.cache.find("name", "mod-log");
+  if (!modlog) return message.reply("mod-log kanalını bulamıyorum.");
+  if (reason.length < 1) return message.reply("Ban sebebini yazmalısın.");
+  if (message.mentions.users.cache.size < 1)
+    return message.reply("Kimi banlayacağını yazmalısın.").catch(console.error);
 
-  if (!message.guild.member(user).members.bannable) return message.reply('Yetkilileri banlayamam.');
+  if (!message.guild.member(user).members.bannable)
+    return message.reply("Yetkilileri banlayamam.");
   message.guild.members.ban(user, 2);
 
   const embed = new Discord.MessageEmbed()
-    .setColor(0x00AE86)
+    .setColor(0x00ae86)
     .setTimestamp()
-    .addField('Eylem:', 'Ban')
-    .addField('Kullanıcı:', ${user.username}#${user.discriminator} (${user.id}))
-    .addField('Yetkili:', ${message.author.username}#${message.author.discriminator})
-    .addField('Sebep', reason);
+    .addField("Eylem:", "Ban")
+    .addField("Sebep", reason);
   return guild.channels.cache.get(modlog.id).send(embed);
 };
 
@@ -38,7 +42,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'ban',
-  description: 'İstediğiniz kişiyi banlar.',
-  usage: 'ban [kullanıcı] [sebep]'
+  name: "ban",
+  description: "İstediğiniz kişiyi banlar.",
+  usage: "ban [kullanıcı] [sebep]"
 };
