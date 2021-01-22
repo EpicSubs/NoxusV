81350de7523fabe95e06ffcdfa91857e950a21f5
Glitch (hello-express) <none>
2021-01-22 10:33:02 +0000

    🍚📯 Checkpoint ./komutlar/servericon.js:19428437/517:21810194/1

--- komutlar/servericon.js
+++ komutlar/servericon.js
@@ -1,27 +1,13 @@
 const Discord = require("discord.js");
 
 exports.run = (client, message, params) => {
-  if (!message.guild) {
-    const ozelmesajuyari = new Discord.MesageEmbed()
-      .setColor(0xff0000)
-      .setTimestamp()
-      .setAuthor(message.author.username, message.author.avatarURL())
-      .addField(
-        ":warning: Uyarı :warning:",
-        "`sunucuresmi` adlı komutu özel mesajlarda kullanamazsın."
-      );
-    return message.author.send(ozelmesajuyari);
-  }
-  if (message.channel.type !== "dm") {
+  
     const sunucubilgi = new Discord.MessageEmbed()
-      .setAuthor(message.guild.name)
-      .setColor(3447003)
-      .setTimestamp()
-      .setDescription("")
-      .setImage(`${message.guild.iconURL()} `);
-    return message.channel.send(sunucubilgi);
+      .setTitle(message.guild.name)
+      .setImage(`${message.guild.iconURL()}`);
+message.channel.send(sunucubilgi);
   }
-};
+
 
 exports.conf = {
   enabled: true,
